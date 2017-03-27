f51c5bb16153bee4c5bb34c88be50e0ababca201
panQ <361671402@qq.com>
2017-03-27 15:05:30 +0800

    修改本周详情购物车数量不显示

--- classify/js/weekoffer.js
+++ classify/js/weekoffer.js
@@ -1,4 +1,4 @@
-app.controller('weekctrl', ["$http",'$scope','cartService', '$state', function($http,$scope, cartService, $state){
+app.controller('weekctrl', ["$http",'$scope','cartService', '$state', '$rootScope',function($http,$scope, cartService, $state, $rootScope){
 	$http.get('http://api5.wochu.cn/client/v1/goods/SearchAndCategoryByCategoryId?parameters=%7B%22orderId%22%3A0%2C%22categoryId%22%3A648%7D').success(function(data){
 
 		var self = this;
@@ -8,6 +8,7 @@ app.controller('weekctrl', ["$http",'$scope','cartService', '$state', function($
 	$scope.addGood = function () {
 		cartService.addGoods(this.item);
 		console.log(cartService.getAllGoods()) ;
+		$rootScope.num = cartService.getGoodsLength();
 	}
 	$scope.showDetail = function (){
 		console.log(this.item);
