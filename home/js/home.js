1a63e3a6522f715da3f36fc1d7c05ea773fba3d6
panQ <361671402@qq.com>
2017-03-27 20:06:03 +0800

    最终版本2

--- home/js/home.js
+++ home/js/home.js
@@ -4,7 +4,7 @@ app.controller('homeCtrl', ['$scope', '$rootScope', '$http', '$timeout', 'cartSe
 		console.log(this.item1);
 		$state.go("detail", { id: JSON.stringify(this.item1) })
 	}
-
+	
 	//轮播
 	$timeout(function() {
 		var mySwiper = new Swiper('.swiper-container', {
@@ -32,15 +32,40 @@ app.controller('homeCtrl', ['$scope', '$rootScope', '$http', '$timeout', 'cartSe
 
 	//点击添加商品事件
 	$scope.addGood = function() {
-		console.log(this.item1);
 		//保存商品数据
 		cartService.addGoods(this.item1);
-		console.log(cartService.getAllGoods());
 		cartService.getGoodsLength();
 		$rootScope.num = cartService.getGoodsLength();
 	}
 	
-	
+	$timeout(function () {
+		$(function() {
+			var offset = $("#end").offset();
+			console.log(offset)
+			$(".addcar").click(function(event) {
+				var addcar = $(this);
+				console.log($(this))
+				var img = addcar.parent().parent().find('img').attr('src');
+				var flyer = $('<img class="u-flyer" src="' + img + '">');
+				console.log(flyer)
+				flyer.fly({
+					start: {
+						left: event.clientX, //开始位置（必填）#fly元素会被设置成position: fixed 
+						top: event.clientY //开始位置（必填） 
+					},
+					end: {
+						left: offset.left + 10, //结束位置（必填） 
+						top: offset.top + 10, //结束位置（必填） 
+						width: 0, //结束时宽度 
+						height: 0 //结束时高度 
+					},
+					onEnd: function() { //结束回调 
+						$("#msg").show().animate({ width: '250px' }, 200).fadeOut(1000); //提示信息 
+					}
+				});
+			});
+		});
+	}, 1000)
 	//footer
 	var index = document.querySelector("#index");
 	var shopping = document.querySelector("#shopping");
--- service.js
+++ service.js
@@ -1,95 +1,94 @@
 // 服务
-app.service('cartService', function () {
-    // 定义数组，用于保存所有购物车数据
-    var arr = [];
-    // 添加商品的方法
-    this.addGoods = function (obj) {
-    	if (arr.length == 0) {
-    		obj.count = 1;
-  			arr.push(obj);
-    	} else {
-    		var l = arr.length;
-    		for (var i = 0; i < l; i++) {
-  				if (arr[i].goodsName == obj.goodsName) {
-  					arr[i].count++;
-  					return;
-  				}
-    		}
-    		obj.count = 1;
-    		arr.push(obj);
-    	}
-    }
-	
-    // 获取所有商品的方法
-    this.getAllGoods = function () {
-        return arr;
-    };
-    
-    //获取数组长度
-    this.getGoodsLength = function () {
-    	return arr.length;
-    }
-    //详情添加商品方法
-    this.addDetailGood = function (obj) {
-    	var l = arr.length;
-    		for (var i = 0; i < l; i++) {
-  				if (arr[i].goodsName == obj.goodsName) {
-  					arr[i].count = obj.count;
-  					return;
-  				}
-    		}
-    	arr.push(obj);
-    }
+app.service('cartService', function() {
+	// 定义数组，用于保存所有购物车数据
+	var arr = [];
+	// 添加商品的方法
+	this.addGoods = function(obj) {
+		if(arr.length == 0) {
+			obj.count = 1;
+			arr.push(obj);
+		} else {
+			var l = arr.length;
+			for(var i = 0; i < l; i++) {
+				if(arr[i].goodsName == obj.goodsName) {
+					arr[i].count++;
+					return;
+				}
+			}
+			obj.count = 1;
+			arr.push(obj);
+		}
+	}
 
-    
-    //删除商品的方法
-    this.delGoods = function (value) {
-    	for (var i = 0; i < arr.length; i ++) {
-    		if (arr[i].goodsName == value) {
-    			arr.splice(i, 1);
-    			return;
-    		}
-    	}
-    };
-    
-    //获取价格
-    this.getPrice = function (id) {
-    	for (var i = 0; i < arr.length; i ++) {
-    		if (arr[i].goodsName == id) {
-    			return arr[i].price * arr[i].count;
-    		}
-    	}
-    };
-    
-    //获取所有价格
-    this.getAllPrice = function () {
-    	var money = 0;
-    	for (var i = 0; i < arr.length; i++) {
-    		var tempMoney = arr[i].price;
-    		var count = arr[i].count;
-    		money += tempMoney * count;
-    	}
-    	return money;
-    }
-    
-    //回到顶部
-    this.returntop = function(){
-			
-			var timer = null;
-			
-			cancelAnimationFrame(timer);
-			
-			timer = requestAnimationFrame(function fn(){
-				var oTop = document.body.scrollTop || document.documentElement.scrollTop;
-				if(oTop > 0){
-					document.body.scrollTop = document.documentElement.scrollTop = oTop - 50;
-					timer = requestAnimationFrame(fn);
-				}else{
-					cancelAnimationFrame(timer);
-				} 
-			})
-		};
-    
-    
-    
+	// 获取所有商品的方法
+	this.getAllGoods = function() {
+		return arr;
+	};
+
+	//获取数组长度
+	this.getGoodsLength = function() {
+		return arr.length;
+	}
+	//详情添加商品方法
+	this.addDetailGood = function(obj) {
+		var l = arr.length;
+		for(var i = 0; i < l; i++) {
+			if(arr[i].goodsName == obj.goodsName) {
+				arr[i].count = obj.count;
+				return;
+			}
+		}
+		arr.push(obj);
+	}
+
+	//删除商品的方法
+	this.delGoods = function(value) {
+		for(var i = 0; i < arr.length; i++) {
+			if(arr[i].goodsName == value) {
+				arr.splice(i, 1);
+				return;
+			}
+		}
+	};
+
+	//获取价格
+	this.getPrice = function(id) {
+		for(var i = 0; i < arr.length; i++) {
+			if(arr[i].goodsName == id) {
+				return arr[i].price * arr[i].count;
+			}
+		}
+	};
+
+	//获取所有价格
+	this.getAllPrice = function() {
+		var money = 0;
+		for(var i = 0; i < arr.length; i++) {
+			var tempMoney = arr[i].price;
+			var count = arr[i].count;
+			money += tempMoney * count;
+		}
+		return money;
+	}
+
+	//回到顶部
+	this.returntop = function() {
+
+		var timer = null;
+
+		cancelAnimationFrame(timer);
+
+		timer = requestAnimationFrame(function fn() {
+			var oTop = document.body.scrollTop || document.documentElement.scrollTop;
+			if(oTop > 0) {
+				document.body.scrollTop = document.documentElement.scrollTop = oTop - 50;
+				timer = requestAnimationFrame(fn);
+			} else {
+				cancelAnimationFrame(timer);
+			}
+		})
+	};
+
+	
+
 });
