76fb7c7421e9a98efeab958f5b6a286f458257f7
panQ <361671402@qq.com>
2017-03-27 20:12:49 +0800

    添加本周详情商品飞入

--- classify/js/weekoffer.js
+++ classify/js/weekoffer.js
@@ -1,4 +1,4 @@
-app.controller('weekctrl', ["$http",'$scope','cartService', '$state', '$rootScope',function($http,$scope, cartService, $state, $rootScope){
+app.controller('weekctrl', ["$http",'$scope','cartService', '$state', '$rootScope','$timeout',function($http,$scope, cartService, $state, $rootScope,$timeout){
 	$http.get('http://api5.wochu.cn/client/v1/goods/SearchAndCategoryByCategoryId?parameters=%7B%22orderId%22%3A0%2C%22categoryId%22%3A648%7D').success(function(data){
 
 		var self = this;
@@ -10,6 +10,34 @@ app.controller('weekctrl', ["$http",'$scope','cartService', '$state', '$rootScop
 		console.log(cartService.getAllGoods()) ;
 		$rootScope.num = cartService.getGoodsLength();
 	}
+	$timeout(function () {
+		$(function() {
+			var offset = $("#end").offset();
+			console.log(offset)
+			$(".addcar").click(function(event) {
+				var addcar = $(this);
+				console.log($(this))
+				var img = addcar.parent().parent().find('img').attr('src');
+				var flyer = $('<img class="u-flyer" src="' + img + '">');
+				console.log(flyer)
+				flyer.fly({
+					start: {
+						left: event.clientX, //开始位置（必填）#fly元素会被设置成position: fixed 
+						top: event.clientY //开始位置（必填） 
+					},
+					end: {
+						left: offset.left + 10, //结束位置（必填） 
+						top: offset.top + 10, //结束位置（必填） 
+						width: 0, //结束时宽度 
+						height: 0 //结束时高度 
+					},
+					onEnd: function() { //结束回调 
+						$("#msg").show().animate({ width: '250px' }, 200).fadeOut(1000); //提示信息 
+					}
+				});
+			});
+		});
+	}, 1000)
 	$scope.showDetail = function (){
 		console.log(this.item);
 		$state.go("detail", {id: JSON.stringify(this.item) })
